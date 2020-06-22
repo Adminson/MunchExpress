3d10296abd1dffd1a8ca7ed32849c378b1699d68
Prithiviraj <prithivirajson@gmail.com>
2020-06-22 14:51:53 +0800

    Add login

--- database/seeds/MenusTableSeeder.php
+++ database/seeds/MenusTableSeeder.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class MenusTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(MenusTableSeeder::class);
+        factory(\App\Models\Menu::class,10)->create();
     }
 }
